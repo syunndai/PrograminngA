8b01d6e4b79fece14cb3f445a3b756940a881a85
syunndai <150979545+syunndai@users.noreply.github.com>
2023-12-07 10:55:17 +0900

    Add files via upload

---
+++ 4-01/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+'use strict';
+
+document.getElementById('form').onsubmit = function(event){
+  event.preventDefault();
+  const search = document.getElementById('form').word.value;
+  document.getElementById('output').textContent=`「${search}」の検中...`;
+};
