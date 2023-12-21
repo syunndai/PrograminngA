481803ee8478c56223ea9fbc81571a08a3a835e1
syunndai <150979545+syunndai@users.noreply.github.com>
2023-12-21 15:05:16 +0900

    Add files via upload

---
+++ 6-03/script.js
@@ -0,0 +1,29 @@
+'use strict';
+$(document).ready(function(){
+
+//ファイルの読み込み
+
+  $.ajax({url:'data.json',dataType:'json'})
+  .done(function(data){
+  //console.log(data);
+    data.forEach(function(item,index){
+    if(item.crowded ==='yes'){
+      const idName ='#' + item.id;
+      $(idName).find('.check').addClass('crowded');
+    }
+    });
+  })
+  .fail(function(){
+    window.alert('読み込みエラー');
+  });
+  
+  $('.check').on('click',function(){
+    
+    if($(this).hasClass('crowded')){
+      
+      	$(this).text('残席わずか').addClass('red');
+    }else{
+    	$(this).text('お席あります').addClass('green');
+    }
+  });
+});
